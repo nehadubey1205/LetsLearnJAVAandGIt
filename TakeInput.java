33ea56d5a2a21da9a584add0bbad15ad9eac470e
Neha <nehadubey1205@gmail.com>
2022-08-24 15:06:59 +0530

    taking input from user

---
+++ TakeInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class TakeInput {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.nextLine();
+        float f = sc.nextFloat();
+        System.out.println("FloatNo"+f);
+        System.out.println("Strings"+s);
+        System.out.println("Ineger"+n);
+    }
+    
+}
