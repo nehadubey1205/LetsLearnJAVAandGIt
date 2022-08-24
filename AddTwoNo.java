b69a04e5eda2225efbe1b05bd7c58777c23022a6
Neha <nehadubey1205@gmail.com>
2022-08-24 16:10:26 +0530

    add no

---
+++ AddTwoNo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AddTwoNo {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int ans = x+y;
+        System.out.println(x+y);
+        System.out.println("sum =x+y, that is ="+ans);
+    }
+    
+}
