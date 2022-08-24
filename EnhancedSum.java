a126e244e1d20d1aaec8905ae283c0c3ec08dbaa
Neha <nehadubey1205@gmail.com>
2022-08-25 01:22:47 +0530

    performimg array sum by using for each loop

---
+++ EnhancedSum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class EnhancedSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a[] = {4,5,-5,1};
+        int sum =0;
+        for(int n :a){
+            sum = sum+n;
+            System.out.println(sum);
+        }
+       
+    }
+    
+}
