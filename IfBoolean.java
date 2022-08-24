725602f3a844f4bedc8bcfeeabbf21f614c1583e
Neha <nehadubey1205@gmail.com>
2022-08-24 19:19:27 +0530

    how to use boolean

---
+++ IfBoolean.java
@@ -0,0 +1,11 @@
+public class IfBoolean {
+    public static void main(String[] args){
+        boolean learning = false;
+        if(learning)
+           System.out.println("I am a java programmer");
+        else   
+           System.out.println("Condtion has been falsed So i am not a programmer");
+    }// ye *if* ki condition isliye chali coz boolean m learning m true diya hai.... agar *false* dalte toh else chalta
+    
+    
+}
