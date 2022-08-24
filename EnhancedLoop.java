614065a865167b95c39b4e134afdbef46bfaa8c1
Neha <nehadubey1205@gmail.com>
2022-08-25 01:14:22 +0530

    this if for each loop

---
+++ EnhancedLoop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class EnhancedLoop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a[] = {3,4,5,-6,2};
+        for(int num : a){
+            System.out.println(num);
+        }/* this work as the name here (:) means it is a for each loop. I have made an array and 
+        now i m saying that print no. for each value which is present in an array 
+         */
+    }
+    
+}
