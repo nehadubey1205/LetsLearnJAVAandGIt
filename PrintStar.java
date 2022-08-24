a413a739131fd2e8107d1a3f95532c70e6418c0f
Neha <nehadubey1205@gmail.com>
2022-08-24 23:26:20 +0530

    print star 1 to 10 rows wise

---
+++ PrintStar.java
@@ -0,0 +1,15 @@
+public class PrintStar {
+    public static void main(String[]args){
+        //Scanner sc = new Scanner(System.in);
+        int n ;// rows
+        int noofStar;// star in per row
+        for(n=1; n<=10;n++){
+            for(noofStar =1; noofStar<=n;noofStar++){
+                System.out.print("*");
+            }System.out.println();// this is for line change
+            //System.out.println()
+        }
+    
+    }
+    
+}
