e79d346b1704a3336cde590294ca8b8ee6f58dad
Neha <nehadubey1205@gmail.com>
2022-08-25 01:30:21 +0530

    Enhanved String

---
+++ EnhancedString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class EnhancedString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String st [] ={"c","c++","java","python"};
+        for(String s :st){
+            System.out.println(s);
+        }
+    }
+    
+    
+}
