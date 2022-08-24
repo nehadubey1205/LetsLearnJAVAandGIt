1b6b214b4c0f0a8e650ba0f6791d9db9f2110dbf
Neha <nehadubey1205@gmail.com>
2022-08-24 23:16:19 +0530

    forloop simple

---
+++ SimpleForLoop.java
@@ -0,0 +1,10 @@
+public class SimpleForLoop {
+    public static void main(String[] args){
+        //Scanner sc = new Scanner(System.in);
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
