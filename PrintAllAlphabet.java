7dafd257c0a818f8b6001078b8549b8d02efb464
Neha <nehadubey1205@gmail.com>
2022-08-25 01:03:34 +0530

    all alphabet using char

---
+++ PrintAllAlphabet.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PrintAllAlphabet {
+    public static void main(String[] args){
+        //Scanner sc=  new Scanner(System.in);
+        char ch ;
+        for(ch = 'a'; ch<='z';ch++){
+            System.out.println(ch);
+        }
+    }
+    
+}
