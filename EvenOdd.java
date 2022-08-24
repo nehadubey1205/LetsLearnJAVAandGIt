679a513b9cbb88588a69e64d422d53f017785da5
Neha <nehadubey1205@gmail.com>
2022-08-24 21:20:25 +0530

    Even Odd find

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOdd {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x%2==0)
+           System.out.println("Even no");
+        else   
+           System.out.println("Odd no ");
+      //  int y = sc.nextInt();
+
+
+
+    }
+    
+}
