ec8fbba9a442794cd33a10ea9d8f5e7234aa2f0a
Neha <nehadubey1205@gmail.com>
2022-08-25 23:59:14 +0530

    substring

---
+++ FindSubstring.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FindSubstring {
+    public static void main(String[] args){
+        String str = new String("absc");
+        System.out.println("Subsring ");
+        System.out.println(str.substring(2));
+
+    }
+    /* abc = a   b   c
+             ab  bc
+             abc
+             */ 
+            
+            
+
+}
