ef2cfb8cf765fd6f6c0a2d13418562dc1dc812ac
Neha <nehadubey1205@gmail.com>
2022-08-26 23:05:27 +0530

    this is internal methode to reverse a string

---
+++ InternalMethode.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+import java.util.*;
+public class InternalMethode{
+    public static void main(String[]args){
+        StringBuilder s = new StringBuilder("abcdefg");
+        System.out.println(s.reverse());
+    }
+}
