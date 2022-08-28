e08808cae4ee78b3978358e618c2ad0c99bcec4d
Neha <nehadubey1205@gmail.com>
2022-08-28 22:13:38 +0530

    LowerCase

---
+++ ConvertLowerCase.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ConvertLowerCase {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = s1.toLowerCase();
+        System.out.println(s2);
+        // input - Hi Neha
+        //output - hi neha
+    }
+    
+}
