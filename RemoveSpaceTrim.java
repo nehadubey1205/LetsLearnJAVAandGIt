0c6eb81c07ab60aef676e866ca75a4abcfe8ff86
Neha <nehadubey1205@gmail.com>
2022-08-28 22:09:10 +0530

    trim worked as repalce

---
+++ RemoveSpaceTrim.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class RemoveSpaceTrim {
+    public static void main(String [] args){
+        Scanner sc=  new Scanner(System.in);
+        String s1 = sc.nextLine();
+        System.out.println(s1+" neha");
+        System.out.println(s1.trim()+" are");
+        /*input-we are here
+       we are here neha
+        we are here are */
+    }
+    
+}
