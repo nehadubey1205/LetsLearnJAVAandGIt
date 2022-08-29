b92f52d6decfc60ca6ad5bcad255fc6cdd20afb3
Neha <nehadubey1205@gmail.com>
2022-08-30 03:16:53 +0530

    Get IP Addressn

---
+++ GetOwnIP.java
@@ -0,0 +1,7 @@
+import java.net.InetAddress;
+public class GetOwnIP {
+    public static void main(String[] args) throws Exception{
+        System.out.println(InetAddress.getLocalHost());
+    }
+    
+}
