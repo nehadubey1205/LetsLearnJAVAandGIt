ef483d31fa44ef21af0d9582884b011941e3fce2
Neha <nehadubey1205@gmail.com>
2022-08-29 23:30:17 +0530

    learn to create constructor

---
+++ CreateConstructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class CreateConstructor {
+    CreateConstructor(){
+        System.out.println("I m creating constructor");
+    }
+    public static void main(String[]args){
+        CreateConstructor object = new CreateConstructor();
+    }
+    
+}
