f5ba427a205f9ec167edef18142e5e2a7c9491a1
Neha <nehadubey1205@gmail.com>
2022-08-29 22:36:29 +0530

    static Block excute before main method

---
+++ StaticBlock.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("Main method is executed.");
+    }
+
+    static {
+        System.out.println("Static block is executed before main method.");
+    }
+}
